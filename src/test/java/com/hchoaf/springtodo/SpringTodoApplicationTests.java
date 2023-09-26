d00fa60ffe61b41114681e4e9c1a63632e240716
Hangsun Cho <hchoaf@connect.ust.hk>
2023-09-26 18:31:28 +0800

    initial commit

---
+++ src/test/java/com/hchoaf/springtodo/SpringTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hchoaf.springtodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
